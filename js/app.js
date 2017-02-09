f175ddd993fb4790b5cfe0df1fa1a7c2c094fb55
Mark Staebler <markstaebler@gmail.com>
2017-02-08 20:05:39 -0800

    mondodb on heroku setup
    Included nodemon auto refresh dev server as changes are made. npm run
    dev to start the app in dev mode. Setup and created database using
    mongodb on heroku

---
+++ js/app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const db = require('./db.js')
+
+// Connect to the database
+db.connect()
+
+function uid(){
+  return Math.floor(new Date().valueOf()* Math.random())
+}
+
+function shorten(longURL){
+  return {'longURL':longURL,'shortURL':uid()}
+}
+
+app.get('/', (req, res) => {
+  res.send('hello world')
+})
+
+//read arguments passed to URL and save into JSON object
+app.get('/:tagId', (req, res) => {
+  // res.send('tagId: '+ req.params.tagId)
+  res.send(shorten(req.params.tagId))
+})
+
+app.listen(3000)
+
+//parse URL into a JSON object
+//store JSON object to mongoDB
+//shorten URL
+//Return shortened URL to sender
+//If shortender URL is sent redirect to stored full URL
