8115272e46e93cb576ed9b615b27aedd8e461347
Mark Staebler <markstaebler@gmail.com>
2017-02-23 19:37:30 -0800

    syntax error

--- src/js/app.js
+++ src/js/app.js
@@ -9,7 +9,7 @@ const express = require('express'),
 
 module.exports = require('./db')().then(runApp);
 
-app.listen(process.env.port || 3000);
+app.listen(process.env.PORT || 3000);
 
 function runApp(db){
   app.use(express.static(path.join(__dirname, '../../public')));
